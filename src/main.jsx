7133a5d2f9dd9b2564782a12a751e0242985e655
AdamBatch <adam.batchellor@gmail.com>
2025-03-12 14:39:08 -0600

    More changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import PositionPasser from './positionPasser'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('mainApp')).render(
   <StrictMode>
