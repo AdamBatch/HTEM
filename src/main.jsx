e4c8e8624f61befa6d1517ff57ef4269f17a915c
AdamBatch <adam.batchellor@gmail.com>
2025-03-12 14:04:45 -0600

    Not sure at this point

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import ConcentrationChart from './Concentration/concentrationChart'
-import PositionChart from './position/positionChart'
-import OpticalChart from './optical/opticalChart'
-import XrdChart from './xrd/xrdChart'
 import PositionPasser from './positionPasser'
 
 createRoot(document.getElementById('mainApp')).render(
@@ -13,26 +9,3 @@ createRoot(document.getElementById('mainApp')).render(
   </StrictMode>
 )
 
-// createRoot(document.getElementById('concentrationGraph')).render(
-//   <StrictMode>
-//     <ConcentrationChart />
-//   </StrictMode>
-// )
-
-// createRoot(document.getElementById('positionGraph')).render(
-//   <StrictMode>
-//     <PositionChart />
-//   </StrictMode>
-// )
-
-// createRoot(document.getElementById('opticalGraph')).render(
-//   <StrictMode>
-//     <OpticalChart />
-//   </StrictMode>
-// )
-
-// createRoot(document.getElementById('xrdGraph')).render(
-//   <StrictMode>
-//     <XrdChart />
-//   </StrictMode>
-// )
